5a9049e66a24249659f32b1ca0c790d82cbe6660
Bill Shannon <bill@billshannon.com>
2015-03-28 22:59:18 -0600

    Add contact page to nav bar

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,17 +5,6 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
 
-    # if @contact.save
-    #   ContactsMailer.general_message(@contact).deliver
-    #   render :thanks
-    # else
-    #   render :new
-    # end
   end
-
-  def thanks
-  end
-
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,3 @@
 class Contact < ActiveRecord::Base
 
-  validates :name, :message, :presence => :true
-  validates :email,
-            :presence => :true,
-            :format => {
-                :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-                :message => "must be a valid email address"
-            }
 end
